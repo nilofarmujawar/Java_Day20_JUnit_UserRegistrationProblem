562e6d32c95767bbb5f9425c631463ddaee52950
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-11 16:15:39 +0530

    should clear all the email samples provide separately uc9.

--- src/main/java/com/junit/UserRegistrationProblem.java
+++ src/main/java/com/junit/UserRegistrationProblem.java
@@ -18,12 +18,13 @@ package com.junit;
  *       - NOTE – All rules must be passed
  * UC8 :- Rule4 – Has exactly 1 Special Character
  *        - NOTE – All rules must be passed
+ * UC9 :- Should clear all email samples provided separately
  */
-
 
 /**
  *  import matcher class and pattern class
  */
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -336,5 +337,49 @@ public class UserRegistrationProblem {
         String str8 ="Bridgelabz@1"; //userpassword4
         System.out.println(isValidPassword4(str8));
 
+        ArrayList<String> emails = new ArrayList<String>();
+        emails.add("abc@yahoo.com");
+        emails.add("abc-100@yahoo.com");
+        emails.add("abc.100@yahoo.com");
+        emails.add("abc111@abc.com");
+        emails.add("abc-100@abc.net");
+        emails.add("abc.100@abc.com.au");
+        emails.add("abc@1.com");
+        emails.add("abc#@gmail.com.com");
+        emails.add("abc+100@gmail.com");
+        //Add invalid emails in list
+        emails.add("abc");
+        emails.add("abc@.com.my");
+        emails.add("abc123@gmail.a");
+        emails.add("abc123@.com");
+        emails.add("abc123@.com.com");
+        emails.add(".abc@abc.com");
+        emails.add("abc()*@gmail.com");
+        emails.add("abc@%*.com");
+        emails.add("abc..2002@gmail.com");
+        emails.add("abc.@gmail.com");
+        emails.add("abc@abc@gmail.com");
+        emails.add("abc@gmail.com.1a");
+        emails.add("abc@gmail.com.aa.au");
+        /**
+         *  Regular Expression
+         */
+        String regex = "^(.+)@(.+)$";
+        /**
+         * Compile regular expression to get the pattern
+         */
+        Pattern pattern = Pattern.compile(regex);
+        /**
+         * Iterate emails array list
+         */
+        for(String email : emails){
+            /**
+             * Create instance of matcher
+             */
+            Matcher matcher = pattern.matcher(email);
+            System.out.println(email +" : "+ matcher.matches()+"\n");
+        }
     }
 }
+
+
