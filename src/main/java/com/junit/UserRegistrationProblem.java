5e7f8feed1b0f51fd0a192ebc53f88f354171605
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-03-11 15:09:19 +0530

    Added a method to valid Password with NumericNo uc7.

--- src/main/java/com/junit/UserRegistrationProblem.java
+++ src/main/java/com/junit/UserRegistrationProblem.java
@@ -17,7 +17,7 @@ package com.junit;
  * UC7 :- Rule3 – Should  have at least 1 numeric number in the password
  *       - NOTE – All rules must be passed
  */
-
+ 
 
 /**
  *  import matcher class and pattern class
